d30502e8612fba25839ec474c3a16fd36a8e6303
Shunya Sato <shunya.s@jp.tdk.com>
2018-05-03 13:11:35 +0000

    add script to upload to Adafruit IO

---
+++ upload_adafruit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+reference
+* Getting started with Enviro pHAT
+https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-enviro-phat
+* Adafruit IO: The Internet of Things for Everyone
+https://learn.adafruit.com/adafruit-io/overview
+https://github.com/adafruit/io-client-python/tree/master/examples
+"""
+
+# Import Adafruit IO REST client.
+from Adafruit_IO import Client
+
+from envirophat import light, motion, weather, leds
+
+# define credentials in secret.py file
+# ADAFRUIT_IO_KEY = ""
+from secret import *
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_KEY)
+
+print('Start uploading to Adafruit IO')
+
+lux = light.light()
+temp = weather.temperature()
+press = weather.pressure(unit='hPa')
+aio.send('env1_lux', lux)
+aio.send('env1_temp', temp)
+aio.send('env1_press', press)
+print("Posted to Adafruit IO: {:.2f}, {:.2f}, {:.2f}".format(lux, temp, press))
