2d084900c91109b1afe86bcc0050da5e509cfd3f
Shunya Sato <shunya.s@jp.tdk.com>
2018-11-17 13:16:35 +0900

    integrate aws gateway api

--- upload_adafruit.py
+++ upload_adafruit.py
@@ -8,6 +8,10 @@ https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-enviro-phat
 https://learn.adafruit.com/adafruit-io/overview
 https://github.com/adafruit/io-client-python/tree/master/examples
 """
+import time, json
+from datetime import datetime
+
+import requests
 
 # Import Adafruit IO REST client.
 from Adafruit_IO import Client
@@ -21,12 +25,33 @@ from secret import *
 
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USER, ADAFRUIT_IO_KEY)
-print('Start uploading to Adafruit IO')
 
+#TODO: didn't managed to get api key restriction working
+# currently POST method is set not to require api Key
+# something to do with CORS?
+# https://forums.aws.amazon.com/message.jspa?messageID=658389
+headers = {'x-api-key': AWS_ENDPOINT_APIKEY}
+
+now = datetime.utcnow()
 lux = light.light()
+leds.on()
+r,g,b = light.rgb()
+leds.off()
+acc = motion.accelerometer()
+heading = motion.heading()
 temp = weather.temperature()
 press = weather.pressure(unit='hPa')
+
+print('Start uploading to Adafruit IO')
 aio.send('env1-lux', lux) # 1st argument must be feed key, not feed name
 aio.send('env1-temp', temp)
 aio.send('env1-press', press)
 print("Posted to Adafruit IO: {:.2f}, {:.2f}, {:.2f}".format(lux, temp, press))
+
+payload = {}
+payload['datetime']=now.timestamp() # datetime object is not json stringfyable. Use epoch
+payload['env1-lux']=lux
+payload['env1-temp']=temp
+payload['env1-press']=press
+r = requests.post(AWS_ENDPOINT, json=payload, headers=headers)
+print("Posted to AWS Endpoint: {}".format(r.text))
